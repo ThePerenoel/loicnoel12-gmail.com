f9d1cc7aaa55301fcdf71ffd9d4f151271561d98
Loic NOEL <loicnoel12@gmail.com>
2020-04-05 19:13:16 +0200

    Make homepage responsive

--- page-home.php
+++ page-home.php
@@ -1,12 +1,39 @@
-<?php
+<!DOCTYPE html>
 
-  /**
-    Template Name: Accueil
-   */
+<html lang="fr">
 
-   get_header();
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Paris Yoseikan</title>
+  <link href="https://fonts.googleapis.com/css?family=Maven+Pro&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=EB+Garamond|Questrial&display=swap" rel="stylesheet">
+  <?php wp_head(); ?>
+</head>
 
-?>
+<body>
+
+  <nav class="navbar navbar-expand-md">
+    <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu"
+      aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <?php
+              wp_nav_menu(
+                array(
+                  'container'       => 'div',
+                  'container_class' => 'collapse navbar-collapse',
+                  'container_id'    => 'navbarMenu',
+                  'menu_class'      => 'menu',
+                  'echo'            => true,
+                  'fallback_cb'     => 'wp_page_menu',
+                  'items_wrap'      => '<ul class="navbar-nav navbar-items ml-auto">%3$s</ul>',
+                  'depth'           => 0,
+                  'walker'          => new Mf_theme_nav_menu_walker
+                )
+              );
+            ?>
+  </nav>
 
 <div id="home-background" class="parallax">
   <div class="site-name">
